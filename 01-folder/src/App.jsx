37043fde2d2310234648020aabd647f06bfdf547
tishyajena <tishyajena719@gmail.com>
2025-12-26 21:03:05 +0530

    01-folder-done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+const App = () => {
+  return (
+  <>
+    <div id = 'parent'>
+        <h1 id = 'child1'>One</h1>
+        <h2 id = 'child2'>Two</h2>
+    </div>
+    <div id="uncle">
+        <h3 id = 'cousin1'>Three</h3>
+        <h4 id = 'cousin2'>Four</h4>
+    </div>
+  </>
+  )
+}
+
+export default App
